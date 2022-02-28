43552fa4e9b3178b67526ab2658a0c4f3250e21d
Comma <comma@Commas-MacBook-Pro.local>
2022-02-28 14:35:42 +0700

    Updates

---
+++ precache-manifest.cea4b442d83a5174582900bc058ef11a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c32a06fd2e5b0785099a5b66f478f36",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "aae9a845d5363f06dcef",
+    "url": "/react-app/static/css/2.5bdf3fd0.chunk.css"
+  },
+  {
+    "revision": "0e96f9f798d19e132d5b",
+    "url": "/react-app/static/css/main.f6835998.chunk.css"
+  },
+  {
+    "revision": "aae9a845d5363f06dcef",
+    "url": "/react-app/static/js/2.a60fd2b4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-app/static/js/2.a60fd2b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e96f9f798d19e132d5b",
+    "url": "/react-app/static/js/main.d1fa054d.chunk.js"
+  },
+  {
+    "revision": "d474e8d177b992ad4789",
+    "url": "/react-app/static/js/runtime-main.ad3b0c25.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-app/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/react-app/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/react-app/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/react-app/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/react-app/static/media/slick.f97e3bbf.svg"
+  }
+]);
